8cad33711013364073424fb3c2d34490e89c7efd
eknerph <eknerph@gmail.com>
2024-10-10 21:40:07 -0300

    Criado arquivo para exerc_1.rb para os testes da aula de ruby

---
+++ aula-2/exerc_1.rb
@@ -0,0 +1,8 @@
+print "Digite seu 1º nome e ultimo nome(sobrenome):  "
+name = gets.chomp
+
+print "Agora, Digite sua idade: "
+idade =  gets.chomp.to_i
+
+puts "Hello #{name}, prazer em conhece-lo!"
+puts "E #{name}, sua idade é #{idade}, bom saber!"
