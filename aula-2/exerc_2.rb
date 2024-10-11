446cbd228bcdaa0698d7dfba097110112e2d0daa
eknerph <eknerph@gmail.com>
2024-10-10 21:58:37 -0300

    Adicinado os códigos para o exercício dois da segunda aula

---
+++ aula-2/exerc_2.rb
@@ -0,0 +1,14 @@
+print "Digite o primeiro número: "
+number1 =gets.chomp.to_i
+print "Digite o segundo número: "
+number2 = gets.chomp.to_i
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+
+puts "O resultado da Adição é.........#{addition}"
+puts "O resultado da Subtração é......#{subtraction}"
+puts "O resultado da Multiplicação é..#{multiplication}"
+puts "O resultado da Divisão é........#{division}"
