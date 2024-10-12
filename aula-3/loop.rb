8c02c100dfb49c55d269404768e298690c28561b
eknerph <eknerph@gmail.com>
2024-10-12 08:06:03 -0300

    Realizada alteração no código, aula-3/loop.rb

--- aula-3/loop.rb
+++ aula-3/loop.rb
@@ -1,7 +1,18 @@
+# count = 1
+# loop do 
+#     puts count
+#     break if count == 10
+#     # incrementa a variável count
+#     count += 1
+# end
+
 count = 1
+
 loop do 
     puts count
-    break if count == 10
-    # incrementa a variá count
+    if count == 10
+        break
+    end 
+    # incrementa a variável count
     count += 1
 end
