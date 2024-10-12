6e88e3754119cfa9a7a0563a74819494b945d766
eknerph <eknerph@gmail.com>
2024-10-11 23:37:44 -0300

    Adicionado o código para o estudo de  introdução a ruby, aula-3/for.rb, com outro exemplo

--- aula-3/for.rb
+++ aula-3/for.rb
@@ -1,5 +1,18 @@
-fruits = ['Maçã', 'Uva', 'Morango']
+# fruits = ['Maçã', 'Uva', 'Morango', 'Tomate']
+# count = 1
+
+# for x in fruits
+#     puts x
+#     puts count
+#     count = count + 1
+# end
+
+fruits = ['Maçã', 'Uva', 'Morango', 'Tomate']
+
+fruit = 'Laranja'
 
 for fruit in fruits
     puts fruit
 end
+
+puts fruit
