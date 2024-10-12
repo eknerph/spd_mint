50aa87e88cfabaaa1143b8d6ba58d796eef3828a
eknerph <eknerph@gmail.com>
2024-10-12 08:14:12 -0300

    Adicionado o código para o estudo de  introdução a ruby, aula-3/times.rb

---
+++ aula-3/times.rb
@@ -0,0 +1,9 @@
+2.times do
+    puts 'Estou aprendendo times!'
+end
+
+names = ['João', 'alfredo', 'Juca']
+# Igual so array, o times começa com índice 0
+3.times do |index|
+    puts names[index]
+end
