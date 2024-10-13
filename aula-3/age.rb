3dfc245e9898b6fd68cf0a53da4d5c8c55f926e4
eknerph <eknerph@gmail.com>
2024-10-13 18:57:06 -0300

    Realizada correção sitaxe alteração no código, aula-3/age.rb

--- aula-3/age.rb
+++ aula-3/age.rb
@@ -14,7 +14,7 @@ loop do
         print 'Digite o ano atual: '
         current_year = gets.chomp.to_i
         age = current_year - year_of_birth
-        result = "Quem nasceu no ano de #{year_of_birth}, te #{age} anos em #{current_year}"
+        result = "Quem nasceu no ano de #{year_of_birth}, tem #{age} anos em #{current_year}"
     elsif option == 0
         break if option == 0
     else
