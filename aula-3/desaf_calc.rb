e7945fc9b99f1842f2c463a080b189ef1bc3a546
eknerph <eknerph@gmail.com>
2024-10-14 21:35:09 -0300

    Adicionado código da segunda parte, aula-3/desaf_calc.rb

--- aula-3/desaf_calc.rb
+++ aula-3/desaf_calc.rb
@@ -11,17 +11,31 @@ loop do
     break if option == 0
     
     if[1,2,3,4].include?(option)
-    puts 'DIgite o primeiro número: '
-    number1 = gets.chomp.to_f
+        puts 'DIgite o primeiro número: '
+        number1 = gets.chomp.to_f
 
-    puts 'Digite o segundo número: '
-    number2 = gets.chomp.to_f
+        puts 'Digite o segundo número: '
+        number2 = gets.chomp.to_f
+    end
+
+    case option
+    when 1
+        result = number1 + number2
+    when 2
+        result = number1 - number2
+    when 3
+        result = number1 * number2
+    when 4 
+        result = number1 / number2
+    else 
+        resul = "Opção inválida"
+    end
+
+    system 'clear'
+
+    if result == "Opção inválida"
+        puts result
+    else
+        puts "O resultado é #{result}"
+    end
 end
-
-case option
-when 1
-    result = number1 + number2
-when 2
-    result = number1
-
-system 'clear'
