761e1b8817a85e462e3c35005dc0339dfc6ef2de
eknerph <eknerph@gmail.com>
2024-10-16 23:43:30 -0300

    Adicionado outro código de exemplo de uso para estudo ruby aula-5/method.rb

--- aula-5/method.rb
+++ aula-5/method.rb
@@ -1,7 +1,16 @@
-# talk 
-def 
-    puts 'Olá, como você está?'
+# # talk 
+# def 
+#     puts 'Olá, como você está?'
+# end
+
+# talk
+
+def tal(first_name, last_name)
+    puts "Olá #{firs_name} #{last_name}, como você está?"
 end
 
-talk
-    
+first_name = 'Leonardo'
+last_name = 'Scorza'
+
+
+talk(first_name, last_name)
