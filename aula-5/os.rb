855b30a2e0eff2a04b08b4a420716c5e45788715
eknerph <eknerph@gmail.com>
2024-10-18 21:42:16 -0300

    Adicionadoarquivo os.rb e código de teste para testar o sitema operacional para estudos ruby puro, aula-5

---
+++ aula-5/os.rb
@@ -0,0 +1,14 @@
+require 'os'
+
+def my_os
+  if OS.windows?
+    "Windows"
+  elsif OS.linux?
+    "Linux"
+  elsif OS.mac?
+    "Osx"
+  else
+    "Não consegui identificar"
+  end
+end
+puts "Meu PC possui #{OS.cpu_count} cores, é #{OS.bits} bits e o operacional é #{my_os}"
