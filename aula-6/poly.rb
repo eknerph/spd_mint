15445be00ab1ff4228d1a2c92662ca9cae9b11ef
eknerph <eknerph@gmail.com>
2024-10-19 15:38:59 -0300

    Adicionado arquivo e o código para estudo para ruby, aula-6/poly.rb

---
+++ aula-6/poly.rb
@@ -0,0 +1,47 @@
+class Instrumento
+  def escrever 
+    puts 'Escrevendo'
+  end
+end
+
+class Teclado < Instrumento
+  def escrever
+    puts "tecladooooooo"
+    super
+  end
+end
+
+class Lapis < Instrumento
+  def escrever
+    puts 'Escrevendo à Lápis'
+  end    
+end
+
+class Caneta < Instrumento
+  def escrever
+    puts 'Escrevendo à Caneta'
+  end
+end
+
+teclado = Teclado.new
+lapis = Lapis.new
+caneta = Caneta.new
+
+# puts "lápis:"
+# lapis.escrever
+# puts "caneta:"
+# caneta.escrever
+puts "taclado:"
+teclado.escrever
+
+# instrumento = [Lapis.new, Caneta.new]
+# # Chamamos o método escrever pra qualquer instrumento
+# Instrumentos.each do |instrumento|
+#   Instrumento.escrever
+# end
+
+
+
+
+
+
