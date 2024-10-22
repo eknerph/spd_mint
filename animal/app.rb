67cff47eac0cc9e3e4ddd3d3ccac96bbe2e7fdd4
eknerph <eknerph@gmail.com>
2024-10-21 21:17:17 -0300

    Adicionado itens de código para execução do require_ e adicionado arquivo cachorro.rb para exemplo de continuidade de estudo ruby puro, /aninal/app.rb

--- animal/app.rb
+++ animal/app.rb
@@ -1,9 +1,23 @@
+# 1º teste com error
 # require './animal.rb' # Exemplo que mostra a forma errada de usar
 
 # animal = Animal.new
 # animal.pular
 
-require_relative 'animal'
+# 2º teste
+# require_relative 'animal'
 
+# animal = Animal.new
+# animal.pular
+
+require_relative 'animal'
+require_relative 'cachorro'
+
+puts '--Animal--'
 animal = Animal.new
 animal.pular
+
+puts '--Cachorro--'
+cachorro = Cachorro.new
+cachorro.pular
+cachorro.latir
