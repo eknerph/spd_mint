2e24e8a73fb09a974b82aa4528636fdd508cf46d
eknerph <eknerph@gmail.com>
2024-10-23 22:24:24 -0300

    Adicionado segundo código exemplo ao arquivo para continuidade estudo ruby puro aula-7/atributos.rb

--- aula-7/atributos.rb
+++ aula-7/atributos.rb
@@ -11,3 +11,16 @@
 # dog = Dog.new
 # dog.name = 'Marlon'
 # puts dog.name
+
+
+# Segundo exemplo
+class Dog
+  attr_accessor :name, :age
+end
+
+dog = Dog.new
+dog.name = 'Marlon'
+puts dog.name
+
+dog.age = '1 ano'
+puts dog.age
