c660223f3051401e41270f2e59c2edeac63f2a4b
eknerph <eknerph@gmail.com>
2024-10-23 22:48:13 -0300

    Adicionado código exemplo agora com complemento ao arquivo para continuidade estudo ruby puro aula-7/construtor.rb

--- aula-7/construtor.rb
+++ aula-7/construtor.rb
@@ -1,15 +1,20 @@
 class Person
-  def initialize(name, age)
+  def initialize(name, age, idt, cpf)
     @name = name
     @age = age
+    @idt = idt
+    @cpf = cpf
   end
 
   def check
-    puts "Instancia da classe iniciada com valores:"
+    puts "Instância da classe iniciada com valores:"
     puts "Name = #{@name}"
     puts "Idade = #{@age}"
+    puts "Idt = #{@idt}"
+    puts "Cpf = #{@cpf}"
   end
 end
 
-person = Person.new('João', 12)
+
+person = Person.new('João', 12, '09090909-0', '123456789-20')
 person.check
