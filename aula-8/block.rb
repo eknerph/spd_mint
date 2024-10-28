0f2c16c335faf5c63df344a488878f8d45c8d2e8
eknerph <eknerph@gmail.com>
2024-10-27 23:02:45 -0300

    Adicionado mais dois exemplos de código neste arquivo para continuidade estudo ruby puro aula-8/block.rb

--- aula-8/block.rb
+++ aula-8/block.rb
@@ -1,1 +1,20 @@
-5.times { puts "Exec the block" }
+# 1º exemplo
+# 5.times { puts "Exec the block" }
+
+
+# 2º Exemplo
+# sum = 0
+# numbers = [5, 10, 5]
+# numbers.each { |number| sum += number }
+# puts sum
+
+# 3º exemplo
+foo.each do |key, value|
+    puts "key = #{key}"
+    puts "value = #{value}"
+    puts "key * value = #{key * value}"
+    puts "------"
+end 
+
+
+
