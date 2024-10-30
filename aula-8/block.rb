2ec53d8bfd7deec0380e424b2e44d8094489987f
eknerph <eknerph@gmail.com>
2024-10-29 23:43:13 -0300

    Adicionado pasta e código exemplo ao arquivo para continuidade estudo ruby puro aula-8/lambda.rb

--- aula-8/block.rb
+++ aula-8/block.rb
@@ -55,21 +55,21 @@
 
 # foo ( 'Leonardo' ) { puts "Hellow #{@name}" }
 
-def foo(numbers, &block)
-  if block_given?
-    numbers.each do |key, value|
-      block.call(key, value)
-    end
-  end
-end
+# def foo(numbers, &block)
+#   if block_given?
+#     numbers.each do |key, value|
+#       block.call(key, value)
+#     end
+#   end
+# end
 
-numbers = { 2 => 2, 3=> 3, 4 => 4 }
+# numbers = { 2 => 2, 3=> 3, 4 => 4 }
 
-foo(numbers) do |key, value|
-  puts "#{key} * #{value} = #{key * value}"
-  puts "#{key} + #{value} = #{key + value}"
-  puts "-------"
-end
+# foo(numbers) do |key, value|
+#   puts "#{key} * #{value} = #{key * value}"
+#   puts "#{key} + #{value} = #{key + value}"
+#   puts "-------"
+# end
 
 
 
---
+++ aula-8/lambda.rb
@@ -0,0 +1,15 @@
+# first_lambda = lambda { puts "my first lambda" }
+# first_lambda.call
+# first_lambda.call
+
+first_lambda = -> { puts "my first lambda" }
+first_lambda.call
+
+
+
+
+
+
+
+
+
