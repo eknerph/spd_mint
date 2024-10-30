297ed7d4feb24ceab6f8f2d5086edc63d0d3b359
eknerph <eknerph@gmail.com>
2024-10-29 23:44:15 -0300

    Adicionando mais código exemplo ao arquivo para continuidade estudo ruby puro aula-8/lambda.rb

--- aula-8/lambda.rb
+++ aula-8/lambda.rb
@@ -2,10 +2,14 @@
 # first_lambda.call
 # first_lambda.call
 
-first_lambda = -> { puts "my first lambda" }
-first_lambda.call
+# first_lambda = -> { puts "my first lambda" }
+# first_lambda.call
 
+first_lambda = -> (names){ names.each { |name |puts name } }
 
+names = ["joão", "maria", "pedro"]
+
+first_lambda.call(names)
 
 
 
