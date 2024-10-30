fe6e18e24243e71c04e4dd431172cbdec6bc286f
eknerph <eknerph@gmail.com>
2024-10-29 23:54:00 -0300

    Adicionando mais código exemplo-4 ao arquivo para continuidade estudo ruby puro aula-8/lambda.rb

--- aula-8/lambda.rb
+++ aula-8/lambda.rb
@@ -1,17 +1,35 @@
+# 1º Exemplo
 # first_lambda = lambda { puts "my first lambda" }
 # first_lambda.call
 # first_lambda.call
 
+# 2º exemplo
 # first_lambda = -> { puts "my first lambda" }
 # first_lambda.call
 
-first_lambda = -> (names){ names.each { |name |puts name } }
+# 3º exemplo
+# first_lambda = -> (names){ names.each { |name |puts name } }
 
-names = ["joão", "maria", "pedro"]
+# names = ["joão", "maria", "pedro"]
 
-first_lambda.call(names)
+# first_lambda.call(names)
 
 
+# 4º Exemplo
+my_lambda = lambda do |numbers|
+  index = 0
+  puts 'Número atual + Próximo número'
+  numbers.each do |number|
+    return if numbers[index] == numbers.last
+    puts "(#{numbers[index]}) + (#{numbers[index + 1]})"
+    puts numbers[index] + numbers[index + 1]
+    index += 1
+  end
+end
+
+numbers = [1, 2, 3, 4]
+
+my_lambda.call(numbers)
 
 
 
